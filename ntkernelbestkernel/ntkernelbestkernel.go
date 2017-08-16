aac57d58736145674c14ec78b44deee677e0a867
jmfgdev <jmfgdev@outlook.com>
2017-08-16 15:22:46 -0400

    latest work 3:22

--- ntkernelbestkernel/ntkernelbestkernel.go
+++ ntkernelbestkernel/ntkernelbestkernel.go
@@ -11,3 +11,11 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("---NTKERNELBESTKERNEL---")
+}
