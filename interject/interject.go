fe80e87e5fbea785069ec292f2390e6835e8bf03
jmfgdev <jmfgdev@outlook.com>
2017-08-16 14:30:43 -0400

    add copyright symbols

--- interject/interject.go
+++ interject/interject.go
@@ -1,5 +1,5 @@
 /*
-Copyright (C) <2017>  <jmfgdev@outlook.com>
+Copyright © 2017  <jmfgdev@outlook.com>
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published
 by the Free Software Foundation, either version 3 of the License, or
