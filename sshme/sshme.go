589b7d8c1441e6fe8ac48831bdc0610015465fc3
jmfgdev <jmfgdev@outlook.com>
2017-08-17 05:49:36 -0400

    add sshme

---
+++ sshme/sshme.go
@@ -0,0 +1,32 @@
+/*
+Copyright © 2017  <jmfgdev@outlook.com>
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+package main
+
+import "fmt"
+
+// import "os/exec"
+
+func main() {
+	var username string
+	var ip_or_domain string
+
+	fmt.Println("---SSHME---")
+	fmt.Println("What username@server do you want to login to?")
+	fmt.Printf("Put a server ip or domain here -->  ")
+	fmt.Scanf("%s", &ip_or_domain)
+	fmt.Printf("Put a username here -->  ")
+	fmt.Scanf("%s", &username)
+	fmt.Printf("About to run ssh %s@%s", username, ip_or_domain)
+}
